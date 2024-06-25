d90b3b7890ccd901fe74d95e2d8e0ffc82cc4863
IQ72 <105931001+raihanfaiq72@users.noreply.github.com>
2024-06-25 14:29:17 +0700

    push

--- resources/views/page/admin/dashboard/index.blade.php
+++ resources/views/page/admin/dashboard/index.blade.php
@@ -15,10 +15,18 @@
     <div class="row" style="display: inline-block;">
         <div class="tile_count">
             <div class="col-md-2 col-sm-4  tile_stats_count">
-                <span class="count_top"><i class="fa fa-user"></i> Total Users</span>
-                <div class="count">2500</div>
+                <span class="count_top"><i class="fa fa-user"></i> Total Pegawai</span>
+                <div class="count">{{$pegawai}}</div>
                 <span class="count_bottom"><i class="green">4% </i> From last Week</span>
             </div>
+            @foreach($userJabHitung as $p => $count)
+            <div class="col-md-2 col-sm-4  tile_stats_count">
+                <span class="count_top"><i class="fa fa-user"></i> Total Jab {{$p}}</span>
+                <div class="count green">{{$count}}</div>
+                <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last
+                    Week</span>
+            </div>
+            @endforeach
             <div class="col-md-2 col-sm-4  tile_stats_count">
                 <span class="count_top"><i class="fa fa-clock-o"></i> Average Time</span>
                 <div class="count">123.50</div>
@@ -26,7 +34,7 @@
                     Week</span>
             </div>
             <div class="col-md-2 col-sm-4  tile_stats_count">
-                <span class="count_top"><i class="fa fa-user"></i> Total Males</span>
+                <span class="count_top"><i class="fa fa-user"></i> Total Jab Struk</span>
                 <div class="count green">2,500</div>
                 <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last
                     Week</span>
@@ -35,18 +43,6 @@
                 <span class="count_top"><i class="fa fa-user"></i> Total Females</span>
                 <div class="count">4,567</div>
                 <span class="count_bottom"><i class="red"><i class="fa fa-sort-desc"></i>12% </i> From last
-                    Week</span>
-            </div>
-            <div class="col-md-2 col-sm-4  tile_stats_count">
-                <span class="count_top"><i class="fa fa-user"></i> Total Collections</span>
-                <div class="count">2,315</div>
-                <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last
-                    Week</span>
-            </div>
-            <div class="col-md-2 col-sm-4  tile_stats_count">
-                <span class="count_top"><i class="fa fa-user"></i> Total Connections</span>
-                <div class="count">7,325</div>
-                <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last
                     Week</span>
             </div>
         </div>
@@ -82,70 +78,17 @@
                                 <div class="block">
                                     <div class="block_content">
                                         <h2 class="title">
-                                            <a>Who Needs Sundance When You’ve Got&nbsp;Crowdfunding?</a>
+                                            <a>Pesan khusus</a>
                                         </h2>
                                         <div class="byline">
-                                            <span>13 hours ago</span> by <a>Jane Smith</a>
+                                            <span>12 jam yang lalu</span> by <a>Admin</a>
                                         </div>
-                                        <p class="excerpt">Film festivals used to be do-or-die moments for
-                                            movie makers. They were where you met the producers that could
-                                            fund your project, and if the buyers liked your flick, they’d
-                                            pay to Fast-forward and… <a>Read&nbsp;More</a>
+                                        <p class="excerpt">{{$data}}</a>
                                         </p>
                                     </div>
                                 </div>
                             </li>
-                            <li>
-                                <div class="block">
-                                    <div class="block_content">
-                                        <h2 class="title">
-                                            <a>Who Needs Sundance When You’ve Got&nbsp;Crowdfunding?</a>
-                                        </h2>
-                                        <div class="byline">
-                                            <span>13 hours ago</span> by <a>Jane Smith</a>
-                                        </div>
-                                        <p class="excerpt">Film festivals used to be do-or-die moments for
-                                            movie makers. They were where you met the producers that could
-                                            fund your project, and if the buyers liked your flick, they’d
-                                            pay to Fast-forward and… <a>Read&nbsp;More</a>
-                                        </p>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="block">
-                                    <div class="block_content">
-                                        <h2 class="title">
-                                            <a>Who Needs Sundance When You’ve Got&nbsp;Crowdfunding?</a>
-                                        </h2>
-                                        <div class="byline">
-                                            <span>13 hours ago</span> by <a>Jane Smith</a>
-                                        </div>
-                                        <p class="excerpt">Film festivals used to be do-or-die moments for
-                                            movie makers. They were where you met the producers that could
-                                            fund your project, and if the buyers liked your flick, they’d
-                                            pay to Fast-forward and… <a>Read&nbsp;More</a>
-                                        </p>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="block">
-                                    <div class="block_content">
-                                        <h2 class="title">
-                                            <a>Who Needs Sundance When You’ve Got&nbsp;Crowdfunding?</a>
-                                        </h2>
-                                        <div class="byline">
-                                            <span>13 hours ago</span> by <a>Jane Smith</a>
-                                        </div>
-                                        <p class="excerpt">Film festivals used to be do-or-die moments for
-                                            movie makers. They were where you met the producers that could
-                                            fund your project, and if the buyers liked your flick, they’d
-                                            pay to Fast-forward and… <a>Read&nbsp;More</a>
-                                        </p>
-                                    </div>
-                                </div>
-                            </li>
+                            
                         </ul>
                     </div>
                 </div>
